fb4040e4fc56d3683742143f57a2274a25ddd366
carmineding <carmineding@tencent.com>
2021-09-17 16:50:46 +0800

    add 404 page whoch supports go back to homepage

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    404
+    <router-link to="/keep-account" >返回首页</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'NotFound'
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
