3098b97fb7f932d0831b8873fb536d2444a64119
carmine <dyujie123@gmail.com>
2021-09-17 23:42:58 +0800

    import all svg and adjust nav style

---
+++ src/components/Icon.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg class="icon">
+    <use v-bind:xlink:href="'#' + name"/>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'Icon',
+  props: ['name']
+};
+</script>
+
+<style scoped>
+  .icon{
+    width: 1em; height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+  }
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,29 +1,62 @@
 <template>
-  <div class="nav">
-    <router-link to="/keep-account">
-      <svg>
-        <use xlink:href="#labels"/>
-      </svg>
+  <nav>
+    <router-link to="/keep-account" class="item">
+      <Icon name="keep_account"/>
       记账
     </router-link>
-    |
-    <router-link to="/labels">标签</router-link>
-    |
-    <router-link to="/statistics">统计</router-link>
-  </div>
+    <router-link to="/labels" class="item">
+      <Icon name="label"/>
+      标签
+    </router-link>
+    <router-link to="/statistics" class="item">
+      <Icon name="statistics"/>
+      统计
+    </router-link>
+  </nav>
 </template>
 
 <script lang="ts">
-import x from '@/assets/icons/labels.svg';
 
-console.log(x);
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {
+  importAll(require.context('../assets/icons', true, /\.svg$/));
+} catch (e) {
+  console.log(e);
+}
+
+
 export default {
   name: 'Nav'
 };
 </script>
 
 <style scoped lang="scss">
-.nav {
-  border: 1px solid red;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
+
+nav {
+  display: flex;
+  border: 1px solid red;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+  flex-direction: row;
+  font-size: 12px;
+
+  > .item {
+    width: 33.333333%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 2px 0;
+
+    .icon {
+      width: 32px;
+      height: 32px;
+    }
+  }
+}
+
 </style>
