c6a38738e2a351c822eca685b3e4149d4f09660f
carmine <dyujie123@gmail.com>
2021-09-17 23:48:39 +0800

    color of selected item will change now, edit viewport from m.taobao.com

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav>
-    <router-link to="/keep-account" class="item">
+    <router-link to="/keep-account" class="item" active-class="selected">
       <Icon name="keep_account"/>
       记账
     </router-link>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="label"/>
       标签
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -56,6 +56,9 @@ nav {
       width: 32px;
       height: 32px;
     }
+  }
+  > .item.selected{
+    color: red;
   }
 }
 
